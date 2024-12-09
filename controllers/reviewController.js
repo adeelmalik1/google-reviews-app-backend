87ad22149a87ec95f002134467434ad8607e0f1f
adeelmalik1 <adeeladi0014@gmail.com>
2024-12-09 15:29:18 +0500

    google reviews updates

---
+++ controllers/reviewController.js
@@ -0,0 +1,24 @@
+const getPlaceDetails = require("../helpers/getPlaceDetails");
+const getReviews = require("../helpers/getReviews");
+const AppError = require('../utils/appError');
+const Review = require('../models/reviewModel');
+
+const getLocationReviews = async (req, res, next) => {
+    try {
+        const placeId = await getPlaceDetails(req.body.location);
+        const placeReviews = await getReviews(placeId);
+        console.log(placeReviews);
+        const existingReview = await Review.findOne({ name: placeReviews.name });
+        if (existingReview) {
+            await Review.findByIdAndUpdate(existingReview.id, placeReviews);
+        }
+        else {
+            await Review.insertMany(placeReviews);
+        }
+        res.status(200).json({ message: "Successful", data: placeReviews });
+    } catch (error) {
+        next(new AppError(error.message, 500));
+    }
+};
+
+exports.getLocationReviews = getLocationReviews;
---
+++ helpers/getReviews.js
@@ -0,0 +1,56 @@
+const { Client } = require("@googlemaps/google-maps-services-js");
+const Sentiment = require("sentiment");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Client({});
+const sentiment = new Sentiment();
+
+const apiKey = process.env.GOOGLE_API_KEY;
+
+async function getReviews(placeId) {
+    try {
+        const response = await client.placeDetails({
+            params: {
+                place_id: placeId,
+                key: apiKey,
+                fields: "name,rating,reviews",
+            },
+        });
+
+        if (response.data.status === "OK") {
+            const result = response.data.result;
+
+            const reviews = result.reviews
+                ? result.reviews.map((review) => {
+                    const sentimentResult = sentiment.analyze(review.text);
+                    const sentimentScore = sentimentResult.score;
+                    let sentimentLabel = "neutral";
+
+                    if (sentimentScore > 0) {
+                        sentimentLabel = "positive";
+                    } else if (sentimentScore < 0) {
+                        sentimentLabel = "negative";
+                    }
+
+                    return {
+                        ...review,
+                        sentiment: sentimentLabel,
+                    };
+                })
+                : [];
+
+            return {
+                name: result.name,
+                rating: result.rating,
+                reviews: reviews,
+            };
+        } else {
+            throw new Error("Failed to get reviews. " + response.data.status);
+        }
+    } catch (error) {
+        throw new Error("Error in getReviews: " + error.message);
+    }
+}
+
+module.exports = getReviews;
